dc4a2eaec32e49acc4a2664b8a0a04782c3a14bf
Arthur <arthur.sw@gmail.com>
2022-09-11 15:53:31 +0200

    reduce max size

--- js/Tools/PathTool.js
+++ js/Tools/PathTool.js
@@ -31,7 +31,7 @@
 
       PathTool.emitSocket = false;
 
-      PathTool.maxDraftSize = 500;
+      PathTool.maxDraftSize = 400;
 
       PathTool.computeDraftBounds = function(paths) {
         var bounds, ref;
